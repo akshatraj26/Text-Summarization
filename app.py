21fe0f6b413eca2866433a58163dbf7b615e4baa
akshatraj26 <akshatraj2607@gmail.com>
2023-11-21 00:14:14 +0530

    Added Login System and Contact page

--- app.py
+++ app.py
@@ -1,25 +1,108 @@
-from flask import Flask, url_for, render_template, request
-import json
+from flask import Flask, url_for, render_template, request, redirect, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
 import spacy
 from string import punctuation
 from heapq import nlargest
 from spacy.lang.en.stop_words import STOP_WORDS
 import re
 
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "thismyfirstproject"
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    
+    def __repr__(self):
+        return f"User: {self.name} - {self.email}"
+    
+  
+class Summary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_input = db.Column(db.Text(), nullable=False)
+    summary_text = db.Column(db.Text(), nullable=False)
+    date_created = db.Column(db.DateTime, default= datetime.utcnow)
+    
+    def __repr__(self):
+        return f"User: {self.user_input} - {self.summary_text}"
+    
+class UserQuery(db.Model):
+    id = db.Column(db.Integer, primary_key =True)
+    fname = db.Column(db.String(80), nullable = False)
+    message = db.Column(db.Text, nullable =False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    subject = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def __repr_(self):
+        return f"UserQuery: {self.fname} - {self.message}"
+  
 @app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contacts.html')
+
 @app.route('/signup')
 def signup():
- 
+    
     return render_template('register.html')
 
-@app.route('/register_data', methods = ['POST'])
-def register_data():
+@app.route('/logout')
+def logout():
+    return redirect(url_for('main'))
+    
+
+@app.route('/user_message', methods=["GET", 'POST'])
+def user_message():
+    
+    
+    if request.method == "POST":
+        name = ''
+        email = ''
+        name = request.form['user_name']
+        
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+        print(name, email, subject, message)
+        
+        user_query = UserQuery(fname = name, email=email, subject=subject, message =message)
+        date = UserQuery.query.order_by(UserQuery.date).all()
+        try:
+            db.session.add(user_query)
+            db.session.commit()
+        except:
+            warning = "This email already exists"
+            return render_template('contacts.html', message = warning)
+    return render_template('contacts.html', name =name, email=email, subject=subject, message =message)
+            
+            
+            
+      
+
+@app.route('/register_data', methods = ['GET', 'POST'])
+def Register():
     if request.method == "POST":
         name = request.form['name']
         uname = request.form['uname']
@@ -27,6 +110,21 @@ def register_data():
         pwd = request.form['password']
         
         # print(name, uname, email, pwd)
+        
+        new_user = User(name=name, username =uname, email =email, password=pwd)
+
+
+        try:
+            
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/')
+        except:
+            warning_message = "This username already exists. please choose another one."
+            return render_template('register.html', message =warning_message)
+    
+    else:
+        
         return render_template('register.html', name = name, uname =uname, email=email, pwd=pwd)
         
 
@@ -35,20 +133,34 @@ def signin():
     
     return render_template('login.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form['email'] 
-    pwd = request.form['password']
-    if email and pwd:
-        return json.dumps({'validation' : validateUser(email, pwd)})
+    if request.method == 'POST':
+        user_id = request.form['username']
+        pwd = request.form['password']
+        user = User.query.filter_by(username=user_id).first()
+        
+        if user:
+            if user.password == pwd:
+                session['logged_in'] =True
+                session['user_id'] = user.id
+                flash("Login successful!", 'success')
+                return redirect(url_for('index'))
+
+            else:
+                flash("Incorrect password. Please try again.", 'error')
+                message = "Incorrect password. Please try again."
+                return  render_template('login.html', message = message)
+            
+        else:
+            flash("User does not exist. Please register.", 'error')
+            message = "User does not exist. Please register."
+            return render_template('login.html', message = message)
+    else:
+        return redirect(url_for('signin'))
     
-    return json({'validation': False})
 
-
-def validateUser(username, password):
-    return True
-
-
+    
 
 @app.route('/summarizer', methods=['POST'])
 def summarizer():
@@ -142,6 +254,15 @@ def summarizer():
         clean_output = preprocess_text(data['summary'])
         # Join this so that it won't show in a list format. like this ['hi, hello', 'My name is akshat',]
         clean_output = " ".join(clean_output)
+        
+        summary = Summary(user_input = user_input, summary_text = clean_output)
+        
+        
+        db.session.add(summary)
+        db.session.commit()
+        # return redirect('/')
+        date = Summary.query.order_by(Summary.date_created).all()
+        
             
     return render_template('index.html', data =clean_output)
 
